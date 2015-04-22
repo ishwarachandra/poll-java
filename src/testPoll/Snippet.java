21e94139a8bead17a55c301a281a9c3cc010dc97
ishwarachandra <ishwarachandra@symphonyteleca.com>
2015-04-22 19:45:05 +0530

    data files

---
+++ src/testPoll/Snippet.java
@@ -0,0 +1,16 @@
+
+
+	E:\mavenRepo\mysql\mysql-connector-java\5.1.27\mysql-connector-java-5.1.27.jar
+	E:\softeveryone\jersey-client.jar\jersey-client.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\commons-codec-1.9.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\commons-logging-1.2.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\fluent-hc-4.4.1.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\httpclient-4.4.1.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\httpclient-cache-4.4.1.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\httpclient-win-4.4.1.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\httpcore-4.4.1.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\httpmime-4.4.1.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\jna-4.1.0.jar
+	E:\softeveryone\httpcomponents-client-4.4.1-bin\httpcomponents-client-4.4.1\lib\jna-platform-4.1.0.jar
+
+
